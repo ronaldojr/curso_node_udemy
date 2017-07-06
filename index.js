805dcf034904eddc126ebe8a21a222913d33ffb4
Ronaldo Jr <ronaldo.rjr@gmail.com>
2017-07-06 15:20:41 -0300

    views com ejs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const porta = 3000;
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req,res) => {
-	res.send("home");
-});
+app.set('view engine', 'ejs');
 
+app.get('/', (req,res) => {
+	res.render("index");
+});
 
 app.listen(porta, () => {
 	console.log(`escutando na porta ${porta}`);
