41a7fa9e02f2d4522b414ce4827014d4f166a8a3
Ronaldo Jr <ronaldo.rjr@gmail.com>
2017-07-06 12:44:45 -0300

    requisicao com rota simples

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 let server = http.createServer((req, res) => {
-	res.end("Requisicao OK");
+	
+	let url = req.url;
+
+	if (url == "/um") {
+		res.end("Requisicao UM");	
+	} else if (url  == "/dois") {
+		res.end("Requisicao DOIS");
+	} else {
+		res.end("Rota nao existe, erro 404");
+	}
+	
 });
 
 server.listen(3000);
